6f3c23a2fa635ab4545d026b778ed77007384fea
jun17183 <jun17183@naver.com>
2025-07-24 01:43:53 +0900

    feat. useCallback 구현

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -4,5 +4,3 @@ export function useRef<T>(initialValue: T): { current: T } {
   const [refObj] = useState<{ current: T }>(() => ({ current: initialValue }));
   return refObj;
 }
-
-// Lazy Initialization
