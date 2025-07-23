44c494f7e26debec819216f53f68ba50630fe103
jun17183 <jun17183@naver.com>
2025-07-24 02:03:07 +0900

    feat. useShallowState 구현

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import type { SetStateAction, Dispatch } from "react";
+import { useCallback } from "./useCallback";
 import { shallowEquals } from "../equals";
 
-export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
-  // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-  return useState(initialValue);
+export const useShallowState = <T>(initialValue: T | (() => T)): [T, Dispatch<SetStateAction<T>>] => {
+  const [value, setValue] = useState(initialValue);
+
+  const customSetValue = useCallback((newValue: SetStateAction<T>) => {
+    setValue((prevValue: T) => {
+      // setState(prev => prev + 1) 형태의 업데이트 함수를 사용할 수 있도록 함
+      newValue = typeof newValue === "function" ? (newValue as (prevState: T) => T)(prevValue) : newValue;
+
+      if (!shallowEquals(prevValue, newValue)) {
+        return newValue;
+      }
+
+      return prevValue;
+    });
+  }, []);
+
+  return [value, customSetValue];
 };
