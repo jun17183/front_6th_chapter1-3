113463c041bee33880cbbe265afc744ff91a17f3
jun17183 <jun17183@naver.com>
2025-07-24 01:57:41 +0900

    feat. deepMemo 구현

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,16 @@
 import type { FunctionComponent } from "react";
+import { deepEquals } from "../equals";
 
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  let prevProps: P | null = null;
+  let prevResult: ReturnType<FunctionComponent<P>> | null = null;
+
+  return function MemoizedComponent(props: P) {
+    if (prevProps === null || !deepEquals(prevProps, props)) {
+      prevResult = Component(props);
+      prevProps = props;
+    }
+
+    return prevResult;
+  };
 }
