ace003a19298545d2680fa5fce9133875b730f2c
jun17183 <jun17183@naver.com>
2025-07-24 02:05:34 +0900

    feat. useRouter 구현

--- packages/lib/src/hooks/useRouter.ts
+++ packages/lib/src/hooks/useRouter.ts
@@ -6,7 +6,14 @@ import { useShallowSelector } from "./useShallowSelector";
 const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
 export const useRouter = <T extends RouterInstance<AnyFunction>, S>(router: T, selector = defaultSelector<T, S>) => {
-  // useSyncExternalStore를 사용하여 router의 상태를 구독하고 가져오는 훅을 구현합니다.
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(router);
+
+  return useSyncExternalStore(
+    (callback) => {
+      router.subscribe(callback);
+      return () => router.unsubscribe(callback);
+    },
+    () => shallowSelector(router),
+    () => shallowSelector(router),
+  );
 };
