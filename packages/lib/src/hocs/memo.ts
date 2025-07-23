0f650b1c62063375fccd39b03787e12135cbbcdf
jun17183 <jun17183@naver.com>
2025-07-24 01:57:21 +0900

    feat. memo 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -2,5 +2,15 @@ import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  let prevProps: P | null = null;
+  let prevResult: ReturnType<FunctionComponent<P>> | null = null;
+
+  return function MemoizedComponent(props: P) {
+    if (prevProps === null || !equals(prevProps, props)) {
+      prevResult = Component(props);
+      prevProps = props;
+    }
+
+    return prevResult;
+  };
 }
